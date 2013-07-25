c0e37b85db495088190db0e06ab82086a74deaf4
houjingbiao <Jingzhaohu@163.com>
2013-07-25 05:11:52 -0700

    -update Subsets.cpp

--- solutions/Subsets.cpp
+++ solutions/Subsets.cpp
@@ -32,42 +32,56 @@ public:
 };
 
 //version2: this is a version that just use the index updating
-class Solution {
+class Solution1 {
 public:
-    vector<vector<int>> subsets(vector<int> &S){
-        vector<vector<int>> ret;
-        vector<int> subset;
-        ret.push_back(subset);
-        if(S.size() == 0)
-            return ret;
-        
-        for(int len = 1; len < S.size(); len++){//different length
-            subset.clear();
-            vector<int> subsetIdx; 
-            
-            for(int i = 0; i < len; i++) //initialize index in the S for each element in subset
-                subsetIdx.push_back(i);
-            
-            while(subsetIdx[0]+len-1 < S.size() && subsetIdx[len-1] < S.size()){
-                subset.clear();
-                for(int i = 0; i < len; i++)
-                    subset.push_back(S[subsetIdx[i]]);
-                ret.push_back(subset);
-                
-                int m = len-1;
-                for(; m >= 0; m--){  //for each element in the subset
-                    if(subsetIdx[m]+len-m < S.size()){
-                        subsetIdx[m]++;
-                        for(int k = m+1; k < len; k++)
-                            subsetIdx[k] = subsetIdx[k-1]+1;
-                        break;
-                    }
-                }
-                if(m < 0)
-                    break;
-            }
-        }
-        ret.push_back(S);
-        return ret;
-    }
+	void addToResult(vector<int> &S, vector<int>& subsetIdx,  vector<vector<int>> &ret){
+		vector<int> subset;
+		for(int i = 0; i < subsetIdx.size(); i++)
+			subset.push_back(S[subsetIdx[i]]);
+		ret.push_back(subset);
+	}
+
+	void generateCombination(vector<int>& S, int len, vector<vector<int>> &ret){
+		vector<int> subsetIdx;
+		for(int i = 0; i < len; i++) //initialize index in the S for each element in subset
+			subsetIdx.push_back(i);
+		generateCombination(S, subsetIdx, ret);
+	}
+
+	void generateCombination(vector<int>& S, vector<int>& subsetIdx, vector<vector<int>> &ret){//m is the total length, n is the number of 1 in this binary number
+		addToResult(S, subsetIdx, ret);
+		while(1){
+			int len = subsetIdx.size();
+			int m = len-1;
+			for(; m >= 0; m--){//check and generate the next combination from end to begin
+				if(subsetIdx[m]+len-m < S.size()){
+					subsetIdx[m]++;
+					for(int k = m+1; k < len; k++)
+						subsetIdx[k] = subsetIdx[k-1]+1;
+					addToResult(S, subsetIdx, ret);
+					break;
+				}
+			}
+			if(m < 0)  break;
+		}
+	}
+
+	vector<vector<int>> subsets(vector<int> &S){
+		vector<vector<int>> ret;
+		vector<int> subset;
+		ret.push_back(subset);
+		if(S.size() == 0)
+			return ret;
+
+		for(int len = 1; len < S.size(); len++)//different length
+			generateCombination(S, len, ret);
+
+		ret.push_back(S);
+		return ret;
+	}
 };
+
+
+//version3: dfs
+
+//version4:
