95a8706b69a6c34d2e657bffc423f4d5841d2f47
houjingbiao <Jingzhaohu@163.com>
2013-07-25 07:21:21 -0700

    -add unordered_map + union_find for Longest Consecutive Sequence.cpp

--- solutions/Longest Consecutive Sequence.cpp
+++ solutions/Longest Consecutive Sequence.cpp
@@ -1,3 +1,4 @@
+//version 1: complexity is O(nlogn)
 class Solution {
 public:
     int longestConsecutive(vector<int> &num) {
@@ -6,20 +7,58 @@ public:
         sort(num.begin(), num.end());
         
         int maxLen = 1;
-        vector<int> lengths(num.size(), 1);
+        int curLen = 1;
         for(int i = num.size()-2; i >= 0; i--)
         {
             if(num[i] == num[i+1]-1)
-            {
-                lengths[i] = lengths[i+1]+1;
-                if(maxLen < lengths[i])
-                    maxLen = lengths[i];
-            }
-            else if(num[i] == num[i+1])
-            {
-                lengths[i] = lengths[i+1];
+                curLen++;
+			else if(num[i] < num[i+1]-1){
+                if(maxLen < curLen) maxLen = curLen;
+				curLen = 1;
             }
         }
-        return maxLen;
+        return max(maxLen, curLen);
+    }
+};
+
+//version 2:unordered_map + union_find
+class Solution {
+public:
+	typedef struct Node{
+		int data;
+		int parent;
+	} PSnode;
+
+	int getData(int idx, PSnode* ps, int newparent){
+		int ret = 0;
+		if(ps[idx].parent == idx) 
+			ret = ps[idx].data;
+		else
+			ret = getData(ps[idx].parent, ps, newparent);
+		ps[idx].parent = newparent;
+		return ret;
+	}
+	
+    int longestConsecutive(vector<int> &num) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+		PSnode* PS = new PSnode[num.size()];
+		
+        unordered_map<int, int> mymap;
+		int maxLen = 1;
+		for(int i=0; i<num.size(); i++){
+			if(mymap.count(num[i]) != 0) continue;
+			
+			mymap[num[i]] = i;
+			PS[i].parent = i;
+			PS[i].data = 1;
+			if(mymap.count(num[i]+1) > 0)
+				PS[i].data += getData(mymap[num[i]+1], PS, i);
+			if(mymap.count(num[i]-1) > 0)
+				PS[i].data += getData(mymap[num[i]-1], PS, i);
+				
+			maxLen = maxLen > PS[i].data ? maxLen : PS[i].data;
+		}
+		return maxLen;
     }
 };
