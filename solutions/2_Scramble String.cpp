98a3ae9ca3cee040c362517afcd24186675ee458
houjingbiao <Jingzhaohu@163.com>
2013-09-16 01:57:39 -0700

    -update 2_Scramble String.cpp

--- solutions/2_Scramble String.cpp
+++ solutions/2_Scramble String.cpp
@@ -6,6 +6,61 @@
 //version 2: dfs, only pass small case 8ms
 //version 1: 3-d dp  small case 16ms, large case 620ms
 
+//version 8: dfs + cache(unordered_map) + priming + reserve: small case 12ms, large case 40ms
+typedef string::const_iterator Iterator;
+typedef tuple<Iterator, Iterator, Iterator> Key;
+// 定制一个哈希函数
+namespace std {
+	template<> struct hash<Key> {
+	size_t operator()(const Key & x) const {
+			Iterator first1, last1, first2;
+			std::tie(first1, last1, first2) = x;
+			int result = *first1;
+			result = result * 31 + *last1;
+			result = result * 31 + *first2;
+			result = result * 31 + *(next(first2, distance(first1, last1)-1));
+			return result;
+		}
+	};
+};
+class Solution {
+public:
+	Solution(){
+		cache.reserve(800);
+	}
+	unordered_map<Key, bool> cache;
+	bool isScramble(string s1, string s2) {
+		cache.clear();
+		return isScramble(s1.begin(), s1.end(), s2.begin());
+	}
+	bool isScramble(Iterator first1, Iterator last1, Iterator first2) {
+		auto length = distance(first1, last1);
+		auto last2 = next(first2, length);
+		if (length == 1)
+			return *first1 == *first2;
+		
+		int A[26];
+		std::fill(A, A+26, 0);
+		for(int i = 0; i < length; i++) A[*(first1+i) - 'a']++;
+		for(int i = 0; i < length; i++) A[*(first2+i) - 'a']--;
+		for(int i = 0; i < 26; i++) if (A[i] != 0) return false;
+		
+		for (int i = 1; i < length; ++i)
+			if ((cachedIsScramble(first1, first1 + i, first2)
+				and cachedIsScramble(first1 + i, last1, first2 + i))
+				or (cachedIsScramble(first1, first1 + i, last2 - i)
+				and cachedIsScramble(first1 + i, last1, first2)))
+				return true;
+		return false;
+	}
+	bool cachedIsScramble(Iterator first1, Iterator last1, Iterator first2) {
+		auto key = make_tuple(first1, last1, first2);
+		auto pos = cache.find(key);
+		return (pos != cache.end()) ?
+		pos->second : (cache[key] = isScramble(first1, last1, first2));
+	}
+};
+
 
 //version 7: dfs + cache(unordered_map) + priming: small case 12ms, large case 40ms
 typedef string::const_iterator Iterator;
@@ -64,7 +119,7 @@ typedef string::const_iterator Iterator;
 typedef tuple<Iterator, Iterator, Iterator> Key;
 // 定制一个哈希函数
 namespace std {
-	template<> struct hash<Key> {
+    template<> struct hash<Key> {
 	size_t operator()(const Key & x) const {
 			Iterator first1, last1, first2;
 			std::tie(first1, last1, first2) = x;
@@ -78,6 +133,9 @@ namespace std {
 };
 class Solution {
 public:
+    Solution(){
+        cache.reserve(800);//hjb: this makes the program quick about 10 ms
+    }
 	unordered_map<Key, bool> cache;
 	bool isScramble(string s1, string s2) {
 		cache.clear();
@@ -104,7 +162,6 @@ public:
 		pos->second : (cache[key] = isScramble(first1, last1, first2));
 	}
 };
-
 //version 5: dfs + priming + cache(map) : small case 12ms, large case 52ms
 class Solution {
 private:
