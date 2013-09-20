a0b7726e6748ef4a0ad02e67585f28a4a1447b90
houjingbiao <Jingzhaohu@163.com>
2013-09-20 02:00:15 -0700

    - update Gray Code.cpp

--- solutions/Gray Code.cpp
+++ solutions/Gray Code.cpp
@@ -1,27 +1,29 @@
+//version 2: get the ith gray code from only i
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         vector<int> ret;
-        if(n == 0)
-        {
-            ret.push_back(0);
-            return ret;
-        }
-            
-        int num = 0;
-        ret.push_back(num);
         int count = 1 << n;
-        for(int i = 0; i < count-1; i++)
-        {
+        for(int i = 0; i < count; i++)
+            ret.push_back(i^(i>>1));
+        return ret;
+    }
+};
+
+//version 1: to get the (i+1)th gray code from the ith gray code and i, its drawback is that it have to be start from the first gray code
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ret;
+        int gray = 0;
+        ret.push_back(gray);
+        int count = 1 << n;
+        for(int i = 1; i < count; i++) {
             int mask = 1;
-            while((i & mask) != 0)
-            {
+            while(((i-1) & mask) != 0)
                 mask = mask << 1;
-            }
-            num = (num^mask);
-            ret.push_back(num);
+            gray = gray^mask;
+            ret.push_back(gray);
         }
         return ret;
     }
